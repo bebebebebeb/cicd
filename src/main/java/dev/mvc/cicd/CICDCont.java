02af388d7f413339762b6864938601139f28e2f1
bebebebebeb <syc0726@naver.com>
2023-12-14 15:18:03 +0900

    CICD 테스트

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -9,22 +9,31 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class CICDCont {
-  // http://localhost:9091/cicd?sw=1
   // http://localhost:9091/cicd?sw=-1
+  // http://localhost:9091/cicd?sw=1
   // http://localhost:9091/cicd?sw=0
+  // http://localhost:9091/cicd?sw=2
+  // http://localhost:9091/cicd?sw=3
+  // http://3.38.181.40:9091/cicd?sw=3
   @GetMapping("/cicd")
   public String cicd(@RequestParam(defaultValue = "1") Integer sw) {
     String msg = "";
     
-    if (sw == 1) {
-      msg = "/cicd 호출됨, info 로그";
-      log.info(msg);
-    } else if (sw == -1) {
+    if (sw == -1) {
       msg = "/cide 호출됨, error 로그";
       log.error(msg);
     } else if (sw == 0) {
       msg = "/cide 호출됨, warn 로그";
       log.warn(msg);
+    } else if (sw == 1) {
+      msg = "/cicd 호출됨, info 로그";
+      log.info(msg);
+    } else if (sw == 2) {
+      msg = "/cide workflow 테스트";
+      log.info(msg);
+    } else if (sw == 3) {
+      msg = "/cicd Github action + EC2 최종 테스트 1";
+      log.info(msg);
     }
     
     return "<h3>" + msg + "</h3>";
